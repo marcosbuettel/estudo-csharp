9b46b35eee6008f1eec35eef79070a68386b866c
marcosbuettel <marcosbuettel@yahoo.com.br>
2022-06-28 15:19:58 -0300

    Get e Set

---
+++ 05GetSet/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    Pessoa joao = new Pessoa();
+    joao.Nome = "João";
+    joao.Idade = 30;
+
+    Console.WriteLine("Nome: " + joao.Nome + " Idade: " + joao.Idade);
+  }
+}
